08dbf87702b4df5e883dcf66af6ced70cef54add
nazmifeeroz <nazmifeeroz@hotmail.com>
2018-05-21 05:57:59 +0800

    install cors

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 database = {
   users: [
     {
       id: 1,
       name: 'John',
+      email: 'john@email.com',
+      password: 'cookies',
       age: 30
     },
     {
       id: 2,
       name: 'Sally',
+      email: 'sally@email.com',
+      password: 'bananas',
       age: 6
     },
     {
       id: 3,
       name: 'Mary',
+      email: 'mary@email.com',
+      password: 'apples',
       age: 31
     },
   ]
 }
 
 app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.json('this is working')

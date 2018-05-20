37f078f482bc1807653b8fe38b9ec0eab1f084e7
nazmifeeroz <nazmifeeroz@hotmail.com>
2018-05-21 05:41:15 +0800

    play around with post express and json

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
-const express = require('express');
-
+const bodyParser = require('body-parser')
+const express = require('express')
 const app = express()
 
+database = {
+  users: [
+    {
+      id: 1,
+      name: 'John',
+      age: 30
+    },
+    {
+      id: 2,
+      name: 'Sally',
+      age: 6
+    },
+    {
+      id: 3,
+      name: 'Mary',
+      age: 31
+    },
+  ]
+}
+
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
-  res.send('this is working');
+  res.json('this is working')
+})
+
+app.post('/signin', (req, res) => {
+  if (req.body.name === database.users[0].name) {
+    res.json({
+      "success": "Name Found"
+    }) } else {
+      res.status(400).json({"success": "Name not found"})
+    }
+
 })
 
 app.listen(3000, () => {
-  console.log('server running on port 3000');
+  console.log('server running on port 3000')
 })
